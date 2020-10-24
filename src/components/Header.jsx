e07f59acb02dc32150e0ed9571f165f1bb1f6d44
suryamhmmd <uya_25@yahoo.com>
2020-10-24 12:28:53 +0700

    using useState, useEffect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 // import {Link} from 'react-router-dom'
 import {Link, animateScroll as scroll} from 'react-scroll'
 import {
@@ -11,141 +11,263 @@ import {
 import Brand from '../img/logo.png'
 // import Content from './Content';
 // var prevScrollpos = window.pageYOffset;
-class Header extends Component {
+// class Header extends Component {
 
-    state ={
-        isOpen: false,
-        prevScrollpos: window.pageYOffset,
-        top: 0,
-        backgroundNavbar: false
-    }
+//     state ={
+//         isOpen: false,
+//         prevScrollpos: window.pageYOffset,
+//         top: 0,
+//         backgroundNavbar: false
+//     }
 
-    componentDidMount(){
+//     componentDidMount(){
         
 
+//         window.addEventListener('scroll', ()=>{
+//             // var prevScrollpos = window.pageYOffset
+//             var currentScrollPos = window.pageYOffset;
+//             // console.log(window.scrollY)
+//             if (this.state.prevScrollpos > currentScrollPos) {
+//                 // this.setState({top: 0})
+//                 if(window.scrollY >= 110){
+//                     this.setState({backgroundNavbar: true})
+//                 }else{
+//                     this.setState({backgroundNavbar: false})
+                    
+//                 }
+//             } else {
+//                 // this.setState({top: -60})
+//                 if(window.scrollY >= 110){
+//                     this.setState({backgroundNavbar: true})
+//                 }else{
+//                     this.setState({backgroundNavbar: false})
+//                 }
+//             }
+//             this.setState({prevScrollpos: currentScrollPos})
+//             // prevScrollpos = currentScrollPos;
+//         })
+//     }
+
+//     // componentWillUnmount(){
+//     //     window.removeEventListener('scroll', null)
+//     // }
+
+//     toogle = ()=>{
+//         this.setState({
+//             isOpen: !this.state.isOpen
+//         })
+        
+//     }
+
+//     scrollToTop = () => {
+//         scroll.scrollToTop();
+//     }
+
+//     render() {
+//         return (
+//             <Navbar className={
+//                 this.state.backgroundNavbar ? 'navbar fixed-top navbar-light bg-light shadow-sm p-3 bg-white navbar-scrolled' :
+//                 'navbar fixed-top navbar-light bg-transparent p-3 navbar-default'} light expand="md">
+//                 <Link to="" className="navbar-brand icon-img"
+//                     onClick={this.scrollToTop}
+//                 ><span><img src={Brand} alt=""/></span></Link>
+//                 <NavbarToggler onClick={this.toogle} />
+//                 <Collapse isOpen={this.state.isOpen} navbar>
+                    
+//                 <Nav className="ml-auto" navbar>
+//                     <NavItem>
+//                         <Link className="nav-link"
+//                             activeClass="active"
+//                             to="section_about"
+//                             spy={true}
+//                             smooth={true}
+//                             offset={-70}
+//                             duration={800}
+//                             style={{cursor:'pointer'}}
+//                         >
+//                             About
+//                         </Link>
+//                     </NavItem>
+//                     <NavItem>
+//                         <Link className="nav-link"
+//                             activeClass="active"
+//                             to="section_experience"
+//                             spy={true}
+//                             smooth={true}
+//                             offset={-70}
+//                             duration={800}
+//                             style={{cursor:'pointer'}}
+//                         >
+//                             Experience
+//                         </Link>
+//                     </NavItem>
+//                     <NavItem>
+//                         <Link className="nav-link"
+//                             activeClass="active"
+//                             to="section_skills"
+//                             spy={true}
+//                             smooth={true}
+//                             offset={-70}
+//                             duration={800}
+//                             style={{cursor:'pointer'}}
+//                         >
+//                             Skills
+//                         </Link>
+//                     </NavItem>
+//                     <NavItem>
+//                         <Link className="nav-link"
+//                             activeClass="active"
+//                             to="section_portofolio"
+//                             spy={true}
+//                             smooth={true}
+//                             offset={-70}
+//                             duration={500}
+//                             style={{cursor:'pointer'}}
+//                         >
+//                             Portofolio
+//                         </Link>
+//                     </NavItem>
+//                     <NavItem>
+//                         <Link className="nav-link"
+//                             activeClass="active"
+//                             to="section_contact"
+//                             spy={true}
+//                             smooth={true}
+//                             offset={-70}
+//                             duration={500}
+//                             style={{cursor:'pointer'}}
+//                         >
+//                             Contact
+//                         </Link>
+//                     </NavItem> 
+//                 </Nav>
+
+//                 </Collapse>
+//             </Navbar>
+//         )
+//     }
+// }
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [prevScrollpos, setPrevScrollpos] = useState(window.pageYOffset);
+    // const [top, setTop] = useState(0);
+    const [backgroundNavbar, setBackgroundNavbar] = useState(false);
+
+    const toogle = () => {
+        setIsOpen(!isOpen);
+    }
+
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+    }
+
+    useEffect( () => {
         window.addEventListener('scroll', ()=>{
             // var prevScrollpos = window.pageYOffset
             var currentScrollPos = window.pageYOffset;
             // console.log(window.scrollY)
-            if (this.state.prevScrollpos > currentScrollPos) {
+            if (prevScrollpos > currentScrollPos) {
                 // this.setState({top: 0})
                 if(window.scrollY >= 110){
-                    this.setState({backgroundNavbar: true})
+                   setBackgroundNavbar(true);
                 }else{
-                    this.setState({backgroundNavbar: false})
+                   setBackgroundNavbar(false);
                     
                 }
             } else {
                 // this.setState({top: -60})
                 if(window.scrollY >= 110){
-                    this.setState({backgroundNavbar: true})
+                    setBackgroundNavbar(true);
                 }else{
-                    this.setState({backgroundNavbar: false})
+                    setBackgroundNavbar(false);
                 }
             }
-            this.setState({prevScrollpos: currentScrollPos})
+           setPrevScrollpos(currentScrollPos);
             // prevScrollpos = currentScrollPos;
         })
-    }
+    },[prevScrollpos])
+    return (
+        <Navbar className={
+            backgroundNavbar ? 'navbar fixed-top navbar-light bg-light shadow-sm p-3 bg-white navbar-scrolled' :
+            'navbar fixed-top navbar-light bg-transparent p-3 navbar-default'} light expand="md">
+            <Link to="" className="navbar-brand icon-img"
+                onClick={scrollToTop}
+            ><span><img src={Brand} alt=""/></span></Link>
+            <NavbarToggler onClick={toogle} />
+            <Collapse isOpen navbar>
+                
+            <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <Link className="nav-link"
+                        activeClass="active"
+                        to="section_about"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={800}
+                        style={{cursor:'pointer'}}
+                    >
+                        About
+                    </Link>
+                </NavItem>
+                <NavItem>
+                    <Link className="nav-link"
+                        activeClass="active"
+                        to="section_experience"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={800}
+                        style={{cursor:'pointer'}}
+                    >
+                        Experience
+                    </Link>
+                </NavItem>
+                <NavItem>
+                    <Link className="nav-link"
+                        activeClass="active"
+                        to="section_skills"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={800}
+                        style={{cursor:'pointer'}}
+                    >
+                        Skills
+                    </Link>
+                </NavItem>
+                <NavItem>
+                    <Link className="nav-link"
+                        activeClass="active"
+                        to="section_portofolio"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        style={{cursor:'pointer'}}
+                    >
+                        Portofolio
+                    </Link>
+                </NavItem>
+                <NavItem>
+                    <Link className="nav-link"
+                        activeClass="active"
+                        to="section_contact"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        style={{cursor:'pointer'}}
+                    >
+                        Contact
+                    </Link>
+                </NavItem> 
+            </Nav>
 
-    // componentWillUnmount(){
-    //     window.removeEventListener('scroll', null)
-    // }
-
-    toogle = ()=>{
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-        
-    }
-
-    scrollToTop = () => {
-        scroll.scrollToTop();
-    }
-
-    render() {
-        return (
-            <Navbar className={
-                this.state.backgroundNavbar ? 'navbar fixed-top navbar-light bg-light shadow-sm p-3 bg-white navbar-scrolled' :
-                'navbar fixed-top navbar-light bg-transparent p-3 navbar-default'} light expand="md">
-                <Link to="" className="navbar-brand icon-img"
-                    onClick={this.scrollToTop}
-                ><span><img src={Brand} alt=""/></span></Link>
-                <NavbarToggler onClick={this.toogle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                    
-                <Nav className="ml-auto" navbar>
-                    <NavItem>
-                        <Link className="nav-link"
-                            activeClass="active"
-                            to="section_about"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={800}
-                            style={{cursor:'pointer'}}
-                        >
-                            About
-                        </Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link className="nav-link"
-                            activeClass="active"
-                            to="section_experience"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={800}
-                            style={{cursor:'pointer'}}
-                        >
-                            Experience
-                        </Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link className="nav-link"
-                            activeClass="active"
-                            to="section_skills"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={800}
-                            style={{cursor:'pointer'}}
-                        >
-                            Skills
-                        </Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link className="nav-link"
-                            activeClass="active"
-                            to="section_portofolio"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                            style={{cursor:'pointer'}}
-                        >
-                            Portofolio
-                        </Link>
-                    </NavItem>
-                    <NavItem>
-                        <Link className="nav-link"
-                            activeClass="active"
-                            to="section_contact"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration={500}
-                            style={{cursor:'pointer'}}
-                        >
-                            Contact
-                        </Link>
-                    </NavItem> 
-                </Nav>
-
-                </Collapse>
-            </Navbar>
-        )
-    }
+            </Collapse>
+        </Navbar>
+    )
 }
 
 export default Header
--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Brand from '../img/logo.png'
 import {Link} from 'react-router-dom'
 import {
@@ -8,45 +8,79 @@ import {
     Nav,
     NavItem} from 'reactstrap';
 
-export class Header2 extends Component {
-    state ={
-        isOpen: false,
-        prevScrollpos: window.pageYOffset,
-        top: 0,
-        backgroundNavbar: false
-    }
+// export class Header2 extends Component {
+//     state ={
+//         isOpen: false,
+//         prevScrollpos: window.pageYOffset,
+//         top: 0,
+//         backgroundNavbar: false
+//     }
 
-    toogle = ()=>{
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
+//     toogle = ()=>{
+//         this.setState({
+//             isOpen: !this.state.isOpen
+//         })
         
+//     }
+    
+//     render() {
+//         return (
+//             <Navbar className="navbar fixed-top navbar-light bg-light shadow-sm p-3 bg-white navbar-scrolled" light expand="md">
+//                 <Link to="/" className="navbar-brand icon-img"
+//                 ><span><img src={Brand} alt=""/></span></Link>
+//                 <NavbarToggler onClick={this.toogle} />
+//                 <Collapse isOpen={this.state.isOpen} navbar>
+                    
+//                 <Nav className="ml-auto" navbar>
+                    
+//                     <NavItem>
+//                         <Link className="nav-link"
+//                             to="/"
+//                             style={{cursor:'pointer'}}
+//                         >
+//                             Home
+//                         </Link>
+//                     </NavItem> 
+//                 </Nav>
+
+//                 </Collapse>
+//             </Navbar>
+//         )
+//     }
+// }
+
+const Header2 = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    // const [prevScrollpos, setPrevScrollpos] = useState(window.pageYOffset);
+    // // const [top, setTop] = useState(0);
+    // const [backgroundNavbar, setBackgroundNavbar] = useState(false);
+
+    const toogle = () => {
+        setIsOpen(!isOpen);
     }
     
-    render() {
-        return (
-            <Navbar className="navbar fixed-top navbar-light bg-light shadow-sm p-3 bg-white navbar-scrolled" light expand="md">
-                <Link to="/" className="navbar-brand icon-img"
-                ><span><img src={Brand} alt=""/></span></Link>
-                <NavbarToggler onClick={this.toogle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                    
-                <Nav className="ml-auto" navbar>
-                    
-                    <NavItem>
-                        <Link className="nav-link"
-                            to="/"
-                            style={{cursor:'pointer'}}
-                        >
-                            Home
-                        </Link>
-                    </NavItem> 
-                </Nav>
+    return (
+        <Navbar className="navbar fixed-top navbar-light bg-light shadow-sm p-3 bg-white navbar-scrolled" light expand="md">
+            <Link to="/" className="navbar-brand icon-img"
+            ><span><img src={Brand} alt=""/></span></Link>
+            <NavbarToggler onClick={toogle} />
+            <Collapse isOpen navbar>
+                
+            <Nav className="ml-auto" navbar>
+                
+                <NavItem>
+                    <Link className="nav-link"
+                        to="/"
+                        style={{cursor:'pointer'}}
+                    >
+                        Home
+                    </Link>
+                </NavItem> 
+            </Nav>
 
-                </Collapse>
-            </Navbar>
-        )
-    }
+            </Collapse>
+        </Navbar>
+    )
 }
 
 export default Header2
